885c77e110119530ed12e4f3370c2d6db4c49fdb
matboua <matbouaoussama@gmail.com>
2025-04-26 17:56:27 +0100

    fix: Update import paths for guest components in the Home page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 "use client";
 import { useState } from "react";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import Header from "./components/guest/Header";
+import Hero from "./components/guest/Hero";
 import Footer from "./components/guest/Footer";
 import Subscribe from "./components/guest/Subscribe";
-import WhySK from "./components/WhySK";
-import OffreSK from "./components/OffreSK";
-import Functionality from "./components/Functionality";
-import Testimony from "./components/Testimony";
+import WhySK from "./components/guest/WhySK";
+import OffreSK from "./components/guest/OffreSK";
+import Functionality from "./components/guest/Functionality";
+import Testimony from "./components/guest/Testimony";
 import Faqs from "./components/guest/Faqs";
-import Steps from "./components/Steps";
+import Steps from "./components/guest/Steps";
 
 export default function Home() {
 	const [isOpenMenu, setIsOpenMenu] = useState(false);
