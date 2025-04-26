b741b3ac4737fbd31f737ca06544e433e32212a7
matboua <matbouaoussama@gmail.com>
2025-04-26 20:59:29 +0100

    fix: Enhance layout structure in dashboard page for improved responsiveness and organization

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,11 +3,17 @@ import Sidebar from "../components/client/Sidebar";
 
 export default function page() {
 	return (
-		<section className="flex">
+		<section className="flex h-dvh overflow-y-hidden">
 			<Sidebar />
-			<section className="flex flex-col gap-15 w-full p-5 pl-[72px] md:pl-5 bg-[#fafafa]">
+			<section className="scroll-auto flex flex-col gap-15 w-full p-5 pl-[72px] md:pl-5 bg-[#fafafa]">
 				<Header />
-				<div className="bg-red-100">x</div>
+				<div className="flex flex-col gap-5">
+					<div className="flex bg-neutral-100 w-full rounded-lg py-20"></div>
+					<div className="flex bg-neutral-100 w-full rounded-lg py-20"></div>
+					<div className="flex bg-neutral-100 w-full rounded-lg py-20"></div>
+					<div className="flex bg-neutral-100 w-full rounded-lg py-20"></div>
+					<div className="flex bg-neutral-100 w-full rounded-lg py-20"></div>
+				</div>
 			</section>
 		</section>
 	);
