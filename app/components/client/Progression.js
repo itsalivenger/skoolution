e97067f7259f52cf7bbcca02af30d1e6846ef38d
matboua <matbouaoussama@gmail.com>
2025-04-29 12:40:00 +0100

    feat: Refactor Progression component to dynamically render subject cards and add subject colors in globals.css

--- app/components/client/Progression.js
+++ app/components/client/Progression.js
@@ -1,19 +1,39 @@
-// Start Health Icons
-
-// End Health Icons
-
-import { faSquareRootVariable } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	ICertificatePaperOutline,
-	IExamMultipleChoiceOutline,
-	IExamQualificationOutline,
-} from "healthicons-react";
-import { Brain } from "lucide-react";
-import Link from "next/link";
 import RadarChart from "./Radarchart";
 
 export default function Progression() {
+	const general = {
+		title: "Moyenne générale",
+		degree: "16.5",
+		color: "skblue",
+	};
+	const cards = [
+		{
+			subject: "Mathématiques",
+			degree: 17.5,
+			rank: 5,
+			color: "math",
+		},
+		{
+			subject: "Physique",
+			degree: 16.5,
+			rank: false,
+			color: "physique",
+		},
+		{ subject: "Chimie", degree: 14, rank: false, color: "chimie" },
+		{ subject: "S.V.T", degree: 15.75, rank: false, color: "svt" },
+		{
+			subject: "Anglais",
+			degree: 18.5,
+			rank: 1,
+			color: "english",
+		},
+		{
+			subject: "Philosophie",
+			degree: 19,
+			rank: 10,
+			color: "philosophie",
+		},
+	];
 	return (
 		<section className="px-0 md:px-5 py-5 flex flex-col gap-6">
 			{/* Title + Cards */}
@@ -27,21 +47,28 @@ export default function Progression() {
 				</div>
 				{/* Cards */}
 				<section className="grid grid-cols-12 gap-y-5 gap-0 sm:gap-5 lg:gap-y-10 lg:gap-10">
-					{/* Card One */}
+					{/* Moyenne générale */}
 					<div className="col-span-12 sm:col-span-6 lg:col-span-4 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
 						<p className="font-semibold text-base sm:text-2xl self-start w-full">
-							Moyenne générale
+							{general.title}
 						</p>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl xl:text-5xl font-semibold">
-								17.5
+						<div
+							className={`relative rounded-full w-1/2 aspect-square bg-conic from-skblue from-${general.degree}% to-neutral-200 to-0% transition-all duration-300`}
+							style={{
+								background: `conic-gradient(var(--color-${general.color}) ${
+									general.degree * 18
+								}deg, #e5e5e5 0deg)`,
+							}}
+						>
+							<div className="absolute top-1/2 left-1/2 -translate-1/2 w-[85%] h-[85%] flex justify-center items-center text-2xl sm:text-md lg:text-2xl xl:text-4xl font-semibold bg-white rounded-full">
+								{general.degree}
 								<span className="text-xs text-neutral-400 font-normal">
 									/20
 								</span>
-							</p>
+							</div>
 						</div>
 					</div>
-					{/* Card Two */}
+					{/* Radar Chart */}
 					<div className="col-span-12 sm:col-span-6 lg:col-span-8 bg-white p-3 pb-0 flex flex-col justify-start items-center shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
 						<p className="font-semibold text-base sm:text-2xl self-start w-full">
 							Compétence développée
@@ -50,93 +77,39 @@ export default function Progression() {
 							<RadarChart />
 						</div>
 					</div>
-					{/* Card 3 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<div className="flex justify-between self-start w-full">
-							<p className="font-semibold">Mathématiques</p>
-							<p className="bg-[#135EA555] font-bold text-[#135EA5] text-[8px] flex justify-center items-center p-1 h-4">
-								TOP 5%
-							</p>
+					{/* Subjects */}
+					{cards.map((card) => (
+						<div
+							key={card.subject}
+							className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200"
+						>
+							<div className="flex justify-between self-start w-full">
+								<p className="font-semibold">{card.subject}</p>
+								{card.rank && (
+									<p
+										className={`bg-${card.color}/33 font-bold text-${card.color} text-[8px] flex justify-center items-center p-1 h-4`}
+									>
+										TOP {card.rank} %
+									</p>
+								)}
+							</div>
+							<div
+								className={`relative rounded-full w-1/2 aspect-square bg-conic from-skblue from-${card.degree}% to-neutral-200 to-0% transition-all duration-300`}
+								style={{
+									background: `conic-gradient(var(--color-${card.color}) ${
+										card.degree * 18
+									}deg, #e5e5e5 0deg)`,
+								}}
+							>
+								<div className="absolute top-1/2 left-1/2 -translate-1/2 w-[85%] h-[85%] flex justify-center items-center text-2xl sm:text-md lg:text-2xl font-semibold bg-white rounded-full">
+									{card.degree}
+									<span className="text-xs text-neutral-400 font-normal">
+										/20
+									</span>
+								</div>
+							</div>
 						</div>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								18
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
-					{/* Card 4 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<p className="font-semibold self-start w-full">Physique</p>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								16.5
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
-					{/* Card 5 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<p className="font-semibold self-start w-full">Chimie</p>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								14
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
-					{/* Card 6 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<p className="font-semibold self-start w-full">S.V.T</p>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								15.75
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
-					{/* Card 7 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<div className="flex justify-between self-start w-full">
-							<p className="font-semibold">Anglais</p>
-							<p className="bg-[#8E24AA55] font-bold text-[#8E24AA] text-[8px] flex justify-center items-center p-1 h-4">
-								TOP 10%
-							</p>
-						</div>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								18.5
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
-					{/* Card 8 */}
-					<div className="col-span-12 sm:col-span-4 xl:col-span-3 bg-white p-3 pb-6 flex flex-col justify-start items-center gap-5 shadow-[3px_3px_10px_0px_rgba(0,_0,_0,_0.1)] border border-neutral-200">
-						<div className="flex justify-between self-start w-full">
-							<p className="font-semibold">Philosophie</p>
-							<p className="bg-[#00606455] font-bold text-[#006064] text-[8px] flex justify-center items-center p-1 h-4">
-								TOP 1%
-							</p>
-						</div>
-						<div className="flex justify-center items-center border-10 border-neutral-300 rounded-full w-1/2 aspect-square">
-							<p className="text-2xl sm:text-xl lg:text-3xl font-semibold">
-								19
-								<span className="text-xs text-neutral-400 font-normal">
-									/20
-								</span>
-							</p>
-						</div>
-					</div>
+					))}
 				</section>
 			</div>
 		</section>
