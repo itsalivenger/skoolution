360a7e23c7b0042eea95277649fb18aacaa16af8
matboua <matbouaoussama@gmail.com>
2025-04-30 02:03:01 +0100

    feat: Add Test1 component and integrate it into the math lessons page

---
+++ app/components/client/testes/Test1.js
@@ -0,0 +1,253 @@
+import { Dot, X } from "lucide-react";
+
+export default function Test1() {
+	const questions = [
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+		{
+			question: "Soit f(x)=x+3x+2x-1 ,Quelle est l’assertion vraie ?",
+			answers: [
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+				"limx-> +oo f(x) = 1",
+			],
+		},
+	];
+	return (
+		<section className="relative w-screen h-dvh px-5 py-5 flex flex-col justify-center items-center gap-6 overflow-hidden bg-neutral-50">
+			<div className="absolute top-5 right-5 flex justify-center items-center p-3 bg-neutral-200 hover:bg-neutral-100 text-neutral-700 border border-neutral-300 rounded-full cursor-pointer">
+				<X />
+			</div>
+			{/* Title + steps */}
+			<div className="flex flex-col sm:flex-row gap-3 w-full md:w-10/12 xl:w-8/12">
+				{/* Title */}
+				<div className="flex flex-col gap-1">
+					<p className="text-lg font-semibold text-nowrap">
+						Limites et continuité
+					</p>
+					<p className="text-xs text-neutral-600 flex items-center">
+						Chapitre 01 <Dot /> Test 1
+					</p>
+				</div>
+				{/* Steps */}
+				<div className="px-3 flex flex-col gap-1.5 items-center justify-start w-full">
+					<div className="self-end font-[500]">
+						20<span className="text-neutral-400">/20</span>
+					</div>
+					<div className="grid grid-cols-20 gap-1 w-full">
+						{questions.map((q, i) => (
+							<div key={i} className="h-1 rounded-full bg-blue-600"></div>
+						))}
+					</div>
+				</div>
+				{/* Steps */}
+			</div>
+			{/* QCM */}
+			<div className="flex flex-col gap-10 bg-white p-5 sm:p-10 rounded-xs w-full md:w-10/12 xl:w-8/12">
+				<div className="flex flex-col gap-3">
+					<p className="text-neutral-600 text-lg font-[500]">Question 20 :</p>
+					<p className="text-lg">Quelle est l’assertion vraie ?</p>
+				</div>
+				<div>
+					{/* Question */}
+					<div className="flex flex-col gap-y-5">
+						<div className="flex gap-2.5 items-center border-2 border-neutral-200 hover:border-skblue cursor-pointer group transition-all duration-300 rounded-xs">
+							<div className="py-2.5 px-4 text-skblue group-hover:text-white bg-blue-100 group-hover:bg-skblue transition-all duration-300 font-[500]">
+								A
+							</div>
+							<div>{"lim(x) -> +oo | f(x) = 1"}</div>
+						</div>
+						<div className="flex gap-2.5 items-center border-2 border-neutral-200 hover:border-skblue cursor-pointer group transition-all duration-300 rounded-xs">
+							<div className="py-2.5 px-4 text-skblue group-hover:text-white bg-blue-100 group-hover:bg-skblue transition-all duration-300 font-[500]">
+								B
+							</div>
+							<div>{"lim(x) -> +oo | f(x) = 2"}</div>
+						</div>
+						<div className="flex gap-2.5 items-center border-2 border-neutral-200 hover:border-skblue cursor-pointer group transition-all duration-300 rounded-xs">
+							<div className="py-2.5 px-4 text-skblue group-hover:text-white bg-blue-100 group-hover:bg-skblue transition-all duration-300 font-[500]">
+								C
+							</div>
+							<div>{"lim(x) -> +oo | f(x) = 3"}</div>
+						</div>
+						<div className="flex gap-2.5 items-center border-2 border-neutral-200 hover:border-skblue cursor-pointer group transition-all duration-300 rounded-xs">
+							<div className="py-2.5 px-4 text-skblue group-hover:text-white bg-blue-100 group-hover:bg-skblue transition-all duration-300 font-[500]">
+								D
+							</div>
+							<div>{"lim(x) -> +oo | f(x) = 4"}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
